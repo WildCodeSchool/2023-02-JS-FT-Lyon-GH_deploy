69b995623991fb3603a06969f0314d6b01c05e37
PierreP <paillard.pierre@gmail.com>
2023-05-09 17:12:15 +0200

    import corrected

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./Main";
+import Main from "./main";
 
 function App() {
   return (
